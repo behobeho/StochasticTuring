04ca41cc8f770798633109f979ff03dfd165e964
Bella <bellahobby32@gmail.com>
2026-02-06 20:49:13 +0000

    Plots final timestep
    and now concentration is time-dependent

--- 2-node turing gillespie.py
+++ 2-node turing gillespie.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-steps = 5000 #number of time steps per trajectory
+steps = 1000 #number of time steps per trajectory
 length = 200 #size of the grid
 
 #initialise reaction parameters
@@ -13,10 +13,10 @@ k3 = 0.51
 k4 = 1.00
 dA = 10  #diffusion constant A
 dI = 1000  #diffusion constant I
-pA = 100   #production of A
+pA = 10   #production of A
 pI = 1  #production of I
 muA = 1  #degradation of A
-muI = 10  #degradation of I
+muI = 1  #degradation of I
 
 #initial conditions
 A0 = 10
@@ -38,7 +38,7 @@ for step in range(1, steps+1):
     concI[:, step] = concI[:, step-1]
 
     for box in range(1, length): #iterate through each compartment in the timestep
-        rT = k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA + muI + dA*concA[box,step] + dI*concI[box,step]#rate total, normalisation factor
+        rT = k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step] + muI*concI[box,step] + dA*concA[box,step] + dI*concI[box,step]#rate total, normalisation factor
         Rand1 = np.random.uniform(0,1) #random number for reaction selection
         Rand2 = np.random.uniform(0,1) #random number for time increment
         
@@ -60,13 +60,13 @@ for step in range(1, steps+1):
         elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA +pI) / rT: #basal production of inhibitor
             concI[box, step] += 1
 
-        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA) / rT and concA[box, step]>0: #basal production of activator
+        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step]) / rT and concA[box, step]>0: #basal production of activator
             concA[box, step] -= 1
         
-        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA + muI) / rT and concI[box, step]>0: #basal production of inhibitor
+        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step]) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step] + muI*concI[box,step]) / rT and concI[box, step]>0: #basal production of inhibitor
             concI[box, step] -= 1
         
-        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA + muI) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA + muI + dA*concA[box,step]) / rT and concA[box, step]>0: #Diffusion of A
+        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step] + muI*concI[box,step]) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step] + muI*concI[box,step] + dA*concA[box,step]) / rT and concA[box, step]>0: #Diffusion of A
             pLR = np.random.uniform(0,1) #probability of moving left or right
             
             if box == length-1: #molecules in the far-right compartment have to diffuse left
@@ -85,7 +85,7 @@ for step in range(1, steps+1):
                 concA[box, step] -= 1
                 concA[box+1, step] += 1
         
-        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA + muI) / rT and concI[box, step]>0: #basal production of inhibitor
+        elif (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step]) / rT < Rand1 <= (k1*concA[box, step] + k2*concI[box, step]*concA[box, step] + k3*concA[box, step]*concI[box, step] + k4*concI[box, step] + pA + pI + muA*concA[box,step] + muI*concI[box,step]) / rT and concI[box, step]>0: #basal production of inhibitor
             pLR = np.random.uniform(0,1)
             if box == length-1: #molecules in the far-right compartment have to diffuse left
                 concA[box, step] -= 1
@@ -95,7 +95,7 @@ for step in range(1, steps+1):
                 concA[box, step] -= 1
                 concA[box+1, step] += 1
 
-            if 0 <= pLR < 0.5 and box != 0: #diffuse to the left with probability p=1/2
+            elif 0 <= pLR < 0.5 and box != 0: #diffuse to the left with probability p=1/2
                 concI[box, step] -= 1
                 concI[box-1, step] += 1
 
@@ -105,14 +105,26 @@ for step in range(1, steps+1):
 
     time[step] = time[step-1] - np.log(Rand2)/rT
 
-#plot heatmap of activator and inhibitor    
+"""
+#plot heatmap of activator 
 sns.heatmap(concA)
 plt.title("Activator Copy Number With Diffusion")
 plt.xlabel("Time (s)")
 plt.ylabel("Compartment")
 plt.show()
 
+#plot heatmap of inhibitor
 sns.heatmap(concI, cmap='mako')
 plt.title("Inhibitor Copy Number With Diffusion")
 plt.xlabel("Time (s)")
 plt.ylabel("Compartment")
+plt.show()
+"""
+
+plt.plot(concA[:, steps], color="#bd467a") 
+plt.plot(concI[:, steps], color="#ed6852") 
+plt.title("Diffusion-Enabled")
+plt.xlabel("Compartment")
+plt.ylabel("Copy Number")
+
+
